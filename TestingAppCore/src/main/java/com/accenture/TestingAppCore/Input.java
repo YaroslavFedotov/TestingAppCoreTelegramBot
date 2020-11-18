2deace0477a7ef7b0d08e45f705657cd88014c5f
YaroslavFedotov <56696549+YaroslavFedotov@users.noreply.github.com>
2020-11-18 12:57:36 +0300

    v_0.3

--- TestingAppCore/src/main/java/com/accenture/TestingAppCore/Input.java
+++ TestingAppCore/src/main/java/com/accenture/TestingAppCore/Input.java
@@ -28,7 +28,7 @@ public class Input {
             return authorizationUserProcess(userInput);
         } else if (userFlow) {
             if (testingModOn) {
-                return testingProcess(userInput);
+                return UserController.testPerformer(userInput);
             } else {
                 return userProcess(userInput); }
         } else if (administrationFlow) {
@@ -75,89 +75,89 @@ public class Input {
     }
 
     private static String registerNewUserProcess(String currentMessage) {
-        if (flowStep == 0) {
-            flowStep++;
-            return (DialogueConstant.REGISTER_LOGIN_MESSAGE_BOT);
-        } else if (flowStep == 1) {
-            user.setLogin(currentMessage);
-            flowStep++;
-            return (DialogueConstant.REGISTER_NAME_MESSAGE_BOT);
-        } else if (flowStep == 2) {
-            user.setName(currentMessage);
-            flowStep++;
-            return (DialogueConstant.REGISTER_PASSWORD_MESSAGE_BOT);
-        } else if (flowStep == 3) {
-            user.setPassword(currentMessage);
-            flowStep++;
-            return (DialogueConstant.REGISTER_STATUS_MESSAGE_BOT);
-        } else {
-            if (currentMessage.equals(DialogueConstant.ADMIN_PASSWORD)) {
-                user.setAdmin_status(true);
-            } else if (currentMessage.equals(DialogueConstant.NO_ADMIN_MESSAGE)) {
-                user.setAdmin_status(false);
-            } else {
-                return (DialogueConstant.MISTAKE_MESSAGE_BOT);
-            }
-            ConnectionDB connectionDB = new ConnectionDB();
-            try {
-                connectionDB.registerUser(user);
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-            user = new User();
-            flowStopper();
-            return (DialogueConstant.REGISTER_COMPLETED_MESSAGE_BOT +
-                    "\n\n" + DialogueConstant.START_MESSAGE_BOT);
+        switch (flowStep) {
+            case 0:
+                flowStep++;
+                return (DialogueConstant.REGISTER_LOGIN_MESSAGE_BOT);
+            case 1:
+                user.setLogin(currentMessage);
+                flowStep++;
+                return (DialogueConstant.REGISTER_NAME_MESSAGE_BOT);
+            case 2:
+                user.setName(currentMessage);
+                flowStep++;
+                return (DialogueConstant.REGISTER_PASSWORD_MESSAGE_BOT);
+            case 3:
+                user.setPassword(currentMessage);
+                flowStep++;
+                return (DialogueConstant.REGISTER_STATUS_MESSAGE_BOT);
+            default:
+                if (currentMessage.equals(DialogueConstant.ADMIN_PASSWORD)) {
+                    user.setAdmin_status(true);
+                } else if (currentMessage.equals(DialogueConstant.NO_ADMIN_MESSAGE)) {
+                    user.setAdmin_status(false);
+                } else {
+                    return (DialogueConstant.MISTAKE_MESSAGE_BOT);
+                }
+                ConnectionDB connectionDB = new ConnectionDB();
+                try {
+                    connectionDB.registerUser(user);
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                user = new User();
+                flowStopper();
+                return (DialogueConstant.REGISTER_COMPLETED_MESSAGE_BOT +
+                        "\n\n" + DialogueConstant.START_MESSAGE_BOT);
         }
     }
 
     private static String authorizationUserProcess(String currentMessage) {
-        if (flowStep == 0) {
-            flowStep++;
-            return (DialogueConstant.REGISTER_LOGIN_MESSAGE_BOT);
-        } else if (flowStep == 1) {
-            user.setLogin(currentMessage);
-            flowStep++;
-            return (DialogueConstant.REGISTER_PASSWORD_MESSAGE_BOT);
-        } else {
-            user.setPassword(currentMessage);
-            user.setAdmin_status(false);
-            ConnectionDB connectionDB = new ConnectionDB();
-
-            ResultSet rs = connectionDB.getUser(user);
-            boolean occurrence = false;
-            try {
-                while (rs.next()) {
-                    occurrence = true;
-                }
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-            if (!occurrence) {
-                user.setAdmin_status(true);
-                rs = connectionDB.getUser(user);
-                boolean adminOccurrence = false;
+        switch (flowStep) {
+            case 0:
+                flowStep++;
+                return (DialogueConstant.REGISTER_LOGIN_MESSAGE_BOT);
+            case 1:
+                user.setLogin(currentMessage);
+                flowStep++;
+                return (DialogueConstant.REGISTER_PASSWORD_MESSAGE_BOT);
+            default:
+                user.setPassword(currentMessage);
+                user.setAdmin_status(false);
+                ConnectionDB connectionDB = new ConnectionDB();
+                ResultSet rs = connectionDB.getUser(user);
+                boolean occurrence = false;
                 try {
                     while (rs.next()) {
-                        adminOccurrence = true;
+                        occurrence = true;
                     }
                 } catch (SQLException throwables) {
                     throwables.printStackTrace();
                 }
-                if (adminOccurrence) {
-                    flowStopper ();
-                    administrationFlow = true;
-                    return DialogueConstant.AUTHORIZATION_ADMIN_MESSAGE_BOT;
-                } else {
-                    flowStopper ();
-                    modesOff();
-                    return DialogueConstant.ABSENCE_USER_MESSAGE_BOT;
-
+                if (!occurrence) {
+                    user.setAdmin_status(true);
+                    rs = connectionDB.getUser(user);
+                    boolean adminOccurrence = false;
+                    try {
+                        while (rs.next()) {
+                            adminOccurrence = true;
+                        }
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                    if (adminOccurrence) {
+                        flowStopper();
+                        administrationFlow = true;
+                        return DialogueConstant.AUTHORIZATION_ADMIN_MESSAGE_BOT;
+                    } else {
+                        flowStopper();
+                        modesOff();
+                        return DialogueConstant.ABSENCE_USER_MESSAGE_BOT;
+                    }
                 }
-            }
-            flowStopper ();
-            userFlow = true;
-            return DialogueConstant.AUTHORIZATION_USER_MESSAGE_BOT;
+                flowStopper();
+                userFlow = true;
+                return DialogueConstant.AUTHORIZATION_USER_MESSAGE_BOT;
         }
     }
 
@@ -190,9 +190,6 @@ public class Input {
             default:
                 return DialogueConstant.MISTAKE_MESSAGE_BOT;
         }
-    }
-    private static String testingProcess (String currentMessage) {
-        return UserController.testPerformer(currentMessage);
     }
 
     private static void logout() {
