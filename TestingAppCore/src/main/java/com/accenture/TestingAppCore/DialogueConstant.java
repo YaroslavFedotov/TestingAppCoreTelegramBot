a0a88fc9e33e9a144b159b8b32139095760adfe2
YaroslavFedotov <56696549+YaroslavFedotov@users.noreply.github.com>
2020-11-20 22:57:57 +0300

    spelling correction

--- TestingAppCore/src/main/java/com/accenture/TestingAppCore/DialogueConstant.java
+++ TestingAppCore/src/main/java/com/accenture/TestingAppCore/DialogueConstant.java
@@ -21,26 +21,26 @@ public class DialogueConstant {
     public static final String MISTAKE_START_MESSAGE_BOT = "для начала работы введите\n" + START_USER;
     public static final String MISTAKE_TYPE_MESSAGE_BOT = "я не умею с этим работать :(";
     public static final String ABSENCE_USER_MESSAGE_BOT = "пользователь не найден";
-    public static final String REGISTER_LOGIN_MESSAGE_BOT = "ввидите логин";
-    public static final String REGISTER_NAME_MESSAGE_BOT = "ввидите имя";
-    public static final String REGISTER_PASSWORD_MESSAGE_BOT = "ввидите пороль";
+    public static final String REGISTER_LOGIN_MESSAGE_BOT = "введите логин";
+    public static final String REGISTER_NAME_MESSAGE_BOT = "введите имя";
+    public static final String REGISTER_PASSWORD_MESSAGE_BOT = "введите пороль";
     public static final String REGISTER_COMPLETED_MESSAGE_BOT = "новый пользователь успешно зарегестрирован";
-    public static final String TESTING_START_MESSAGE_BOT = "для того чтобы пройти тест ввидите его название";
-    public static final String QUESTION_CREATE_AUTHOR_MESSAGE_BOT = "ввидите логин автора вопроса";
-    public static final String QUESTION_CREATE_TYPE_MESSAGE_BOT = "ввидите тип вопроса";
-    public static final String QUESTION_CREATE_DIFFICULTY_MESSAGE_BOT = "ввидите сложность вопроса";
-    public static final String QUESTION_CREATE_TEXT_MESSAGE_BOT = "ввидите текст вопроса";
-    public static final String QUESTION_CREATE_ANSWER_MESSAGE_BOT = "ввидите текст ответа(ов) на вопрос";
-    public static final String QUESTION_CREATE_OPEN_ANSWER_MESSAGE_BOT = "ввидите текст ответа на открытый вопрос";
+    public static final String TESTING_START_MESSAGE_BOT = "для того чтобы пройти тест введите его название";
+    public static final String QUESTION_CREATE_AUTHOR_MESSAGE_BOT = "введите логин автора вопроса";
+    public static final String QUESTION_CREATE_TYPE_MESSAGE_BOT = "введите тип вопроса";
+    public static final String QUESTION_CREATE_DIFFICULTY_MESSAGE_BOT = "введите сложность вопроса";
+    public static final String QUESTION_CREATE_TEXT_MESSAGE_BOT = "введите текст вопроса";
+    public static final String QUESTION_CREATE_ANSWER_MESSAGE_BOT = "введите текст ответа(ов) на вопрос";
+    public static final String QUESTION_CREATE_OPEN_ANSWER_MESSAGE_BOT = "введите текст ответа на открытый вопрос";
     public static final String QUESTION_CREATE_FINISH_MESSAGE_BOT = "вопрос успешно создан";
-    public static final String QUESTION_OPEN_MESSAGE_BOT = "ввидите id открываемого вопроса";
+    public static final String QUESTION_OPEN_MESSAGE_BOT = "введите id открываемого вопроса";
     public static final String QUESTION_DELETE_СOMPLETED_MESSAGE_BOT = "вопрос успешно удалён";
-    public static final String QUESTION_DELETE_MESSAGE_BOT = "ввидите id удаляемого вопроса";
-    public static final String TEST_CREATE_NAME_MESSAGE_BOT = "ввидите название теста";
-    public static final String TEST_OPEN_NAME_MESSAGE_BOT = "ввидите название открываемого теста";
-    public static final String TEST_DELETE_MESSAGE_BOT = "ввидите название удаляемого теста";
+    public static final String QUESTION_DELETE_MESSAGE_BOT = "введите id удаляемого вопроса";
+    public static final String TEST_CREATE_NAME_MESSAGE_BOT = "введите название теста";
+    public static final String TEST_OPEN_NAME_MESSAGE_BOT = "введите название открываемого теста";
+    public static final String TEST_DELETE_MESSAGE_BOT = "введите название удаляемого теста";
     public static final String TEST_DELETE_СOMPLETED_MESSAGE_BOT = "тест успешно удалён";
-    public static final String TEST_CREATE_TEXT_MESSAGE_BOT = "ввидите id вопросов через запятую";
+    public static final String TEST_CREATE_TEXT_MESSAGE_BOT = "введите id вопросов через запятую";
     public static final String TEST_CREATE_FINISH_MESSAGE_BOT = "тест успешно создан";
     public static final String TEST_FINISH_MESSAGE_BOT = "тест завершен";
     public static final String QUESTION_PREPARED_STATEMENT_1 = "сложность: ";
@@ -58,7 +58,7 @@ public class DialogueConstant {
             "  -удалить вопрос\n" + LOGOUT_USER+ "  -перезайти";
     public static final String AUTHORIZATION_USER_MESSAGE_BOT = "вы вошли как пользователь\n" +
             "доступные команды:\n" + TAKE_TEST_USER + "  -пройти тест\n" + LOGOUT_USER + "  -перезайти";
-    public static final String REGISTER_STATUS_MESSAGE_BOT = "ввидите код для одминистраторов" +
+    public static final String REGISTER_STATUS_MESSAGE_BOT = "ввидите код для администраторов" +
             " или 'no' если регистрируетесь как пользователь";
     public static final String START_MESSAGE_BOT = "введити одну из команд:\n" +
             REGISTER_USER + "  -заригестрироваться\n" + LOGIN_USER + "  -войти";
